c64c450b8a8285c65f8c335df38dd6037be30cf3
Lachand-Pascal <valentin@lachand.net>
2023-06-23 11:51:50 +0200

    Update plotter.js

--- js/plotter.js
+++ js/plotter.js
@@ -192,7 +192,7 @@ export function plot() {
         if (nbColumns === 5) {
                 x0 = -1 * (d[document.getElementById("label4").value]/2 + d[document.getElementById("label3").value] +  d[document.getElementById("label2").value] + d[document.getElementById("label1").value]);
             } else if (nbColumns === 7) {
-                x0 = -1 * (d[document.getElementById("label3").value]/2 + d[document.getElementById("label2").value] +  d[document.getElementById("label2").value];
+                x0 = -1 * (d[document.getElementById("label3").value]/2 + d[document.getElementById("label2").value] +  d[document.getElementById("label1").value]);
             } else if (nbColumns === 4) {
                 x0 = -1 * (d[document.getElementById("label3").value] +  d[document.getElementById("label2").value] + d[document.getElementById("label1").value]);
             } else if (nbColumns === 6) {
